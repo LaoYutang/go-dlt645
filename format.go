65dfa88771b38b4f4c515281a80415e1ed69c033
laoyutang <539870188@qq.com>
2025-05-27 15:32:37 +0800

    perf 优化format中的BcdBytes2Float64方法

--- format.go
+++ format.go
@@ -1,55 +1,27 @@
 package dlt645
 
 import (
-	"errors"
-	"fmt"
-	"strconv"
-	"strings"
+	"math"
 )
 
 // 压缩BCD码解析成float64 bcd-小字序BCD字节 format-格式"XXX.XXX" allowNegative-是否需要处理负号
-func BcdBytes2Float64(bcd []byte, format string, allowNegative bool) (float64, error) {
-	// 判断format中"X"数量与bcd是否一致
-	xCount := strings.Count(format, "X")
-	if xCount != len(bcd)*2 {
-		return 0, errors.New("format length is not equal to bcd length")
-	}
-
-	// 将BCD中的小字序字节转为大字序字节
-	bcd = BytesReverse(bcd)
-	// 判断是否为负数
-	var isNegative bool
-	if allowNegative {
-		isNegative = bcd[0]&0x80 > 0
-		bcd[0] = bcd[0] & 0x7F
-	}
-
-	// bcd转换成字符串
-	bcdString := ""
-	for _, value := range bcd {
-		bcdString += fmt.Sprintf("%02X", value)
-	}
-
-	// 将BCD的数值赋值到format中
-	resultString := ""
-	bcdIndex := 0
-	for _, value := range format {
-		if value == 'X' {
-			resultString += string(bcdString[bcdIndex])
-			bcdIndex++
-		} else {
-			resultString += string(value)
+func BcdBytes2Float64(data []byte, digit uint8, allowNegative bool) (result float64) {
+	for i, value := range data {
+		if allowNegative && i == len(data)-1 {
+			if value&0x80 != 0 {
+				value = value & 0x7F
+			} else {
+				allowNegative = false
+			}
 		}
+		result += (float64(value>>4)*10 + float64(value&0x0F)) * math.Pow(10, float64(i*2))
 	}
 
-	// 将字符串转换为float64
-	result, err := strconv.ParseFloat(resultString, 64)
-	if err != nil {
-		return 0, err
-	}
+	result /= math.Pow(10, float64(digit))
 
-	if isNegative {
+	if allowNegative {
 		result = -result
 	}
-	return result, nil
+
+	return
 }
--- format_test.go
+++ format_test.go
@@ -10,118 +10,24 @@ func TestBcdBytes2Float64(t *testing.T) {
 	tests := []struct {
 		name          string
 		bcd           []byte
-		format        string
+		digit         uint8
 		allowNegative bool
 		expected      float64
-		expectError   bool
 	}{
-		{
-			name:          "Valid conversion 1",
-			bcd:           []byte{0x12, 0x94},
-			format:        "XX.XX",
-			allowNegative: false,
-			expected:      94.12,
-			expectError:   false,
-		},
-		{
-			name:          "Valid conversion 2",
-			bcd:           []byte{0x12, 0x94},
-			format:        "XX.XX",
-			allowNegative: true,
-			expected:      -14.12,
-			expectError:   false,
-		},
-		{
-			name:          "Valid conversion 3",
-			bcd:           []byte{0x12, 0x34, 0x56},
-			format:        "XX.XXXX",
-			allowNegative: false,
-			expected:      56.3412,
-			expectError:   false,
-		},
-		{
-			name:          "Valid conversion 4",
-			bcd:           []byte{0x12, 0x34, 0x56, 0x78},
-			format:        "XXXXXX.XX",
-			allowNegative: false,
-			expected:      785634.12,
-			expectError:   false,
-		},
-		{
-			name:          "Valid conversion 5",
-			bcd:           []byte{0x5, 0x96},
-			format:        "XX.XX",
-			allowNegative: true,
-			expected:      -16.05,
-			expectError:   false,
-		},
-		{
-			name:          "Valid conversion 6",
-			bcd:           []byte{0x34, 0x1, 0xA1},
-			format:        "XXXX.XX",
-			allowNegative: true,
-			expected:      -2101.34,
-			expectError:   false,
-		},
-		{
-			name:          "X count less than required",
-			bcd:           []byte{0x12},
-			format:        "XXX",
-			allowNegative: false,
-			expected:      0,
-			expectError:   true,
-		},
-		{
-			name:          "X count more than required",
-			bcd:           []byte{0x12, 0x34},
-			format:        "X",
-			allowNegative: false,
-			expected:      0,
-			expectError:   true,
-		},
-		{
-			name:          "Invalid float string",
-			bcd:           []byte{0x1A},
-			format:        "XX",
-			allowNegative: false,
-			expected:      0,
-			expectError:   true,
-		},
-		{
-			name:          "Single byte BCD",
-			bcd:           []byte{0x01},
-			format:        "X.X",
-			allowNegative: false,
-			expected:      0.1,
-			expectError:   false,
-		},
-		{
-			name:          "Byte order reversal",
-			bcd:           []byte{0xCD, 0xAB},
-			format:        "XXXX",
-			allowNegative: false,
-			expected:      0,
-			expectError:   true,
-		},
+		{"Valid conversion 1", []byte{0x12, 0x94}, 2, false, 94.12},
+		{"Valid conversion 2", []byte{0x12, 0x94}, 2, true, -14.12},
+		{"Valid conversion 3", []byte{0x12, 0x34, 0x56}, 4, false, 56.3412},
+		{"Valid conversion 4", []byte{0x12, 0x34, 0x56, 0x78}, 2, false, 785634.12},
+		{"Valid conversion 5", []byte{0x5, 0x96}, 2, false, 96.05},
+		{"Valid conversion 5", []byte{0x5, 0x96}, 2, true, -16.05},
+		{"Valid conversion 6", []byte{0x34, 0x1, 0xA1}, 1, true, -21013.4},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result, err := dlt645.BcdBytes2Float64(tt.bcd, tt.format, tt.allowNegative)
-			// 错误测试但返回正常结果
-			if tt.expectError {
-				if err == nil {
-					t.Errorf("Expected error but got nil")
-				}
-				return
-			}
-			// 正常测试但返回错误
-			if !tt.expectError && err != nil {
-				t.Errorf("Unexpected error: %v", err)
-				return
-			}
+			result := dlt645.BcdBytes2Float64(tt.bcd, tt.digit, tt.allowNegative)
 			// 正常测试但未返回正确结果
-			if !tt.expectError && result != tt.expected {
+			if result != tt.expected {
 				t.Errorf("Expected %v, got %v", tt.expected, result)
 			}
 		})
